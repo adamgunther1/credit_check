236f06b617eea90d2009cc9ceac5d3196a06d844
adamgunther1 <adamgunther1@gmail.com>
2017-03-19 17:22:40 -0600

    refactors code with methods

--- credit_check.rb
+++ credit_check.rb
@@ -1,43 +1,41 @@
-puts "Please enter your credit card number:"
-card_number = gets.chomp
+def credit_card_input
+  puts "Please enter your credit card number:"
+  card_number = gets.chomp
+  return card_number
+end
 
-summed_digits = []
-valid = false
-
-x = -1
-until card_number[x] == nil
-  digit = card_number[x].to_i
-  if x.even?
-    doubled_digit = digit * 2
-      if doubled_digit > 9
-        doubled_string = doubled_digit.to_s
-        separated_digit_string_1 = doubled_string[0] 
-        separated_digit_string_2 = doubled_string[1]
-        separated_digit_summed = separated_digit_string_1.to_i + separated_digit_string_2.to_i
-        summed_digits.insert(0, separated_digit_summed)
+def luhn_algorithm(card_number)
+  reverse_card_number = card_number.reverse
+  separated_digits_reverse_card_number = reverse_card_number.each_char.to_a
+  i = 0
+  sum = 0
+  summed_digits = []
+  until separated_digits_reverse_card_number[i] == nil
+    digit = separated_digits_reverse_card_number[i]
+    if i.even?
+      digit = digit.to_i
+      summed_digits.insert(i, digit)
+    else
+      doubled_digit = digit.to_i * 2
+      if doubled_digit.to_s.length > 1
+        summed_digit = doubled_digit.to_s[0].to_i + doubled_digit.to_s[1].to_i
+        summed_digits.insert(i, summed_digit)
       else
-        summed_digits.insert(0, doubled_digit)
+        summed_digits.insert(i, doubled_digit)
       end
+    end
+    sum += summed_digits[i]
+    i += 1
+  end 
+  return sum  
+end
+
+def validation(sum)
+  if sum % 10 == 0
+    puts "The number is valid!"
   else
-    summed_digits.insert(0, digit)
+    puts "The number is invalid!"
   end
-  x += -1
 end
 
-print summed_digits
-
- y = 0
- sum = 0
- until y == summed_digits.length
-   sum += summed_digits[y]
-   y += 1
-  end
-
-
-if sum % 10 == 0
-  puts ""
-  puts "The number is valid!"
-else
-  puts ""
-  puts "The number is invalid!"
-end
+validation(luhn_algorithm(credit_card_input()))
